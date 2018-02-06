33d2e2a1103d6c947f3faef7ff964a9874815cd1
Alex Ilich <ailich@mail.usf.edu>
2018-02-06 12:08:24 -0500

    Fixed get_blackfly_timestamp_from_vidtime so that it can account for the camera restarting. No longer extracts frame number from filename. Now depends on the table having all frames and in order that they were recorded as it just filters out rows in table that don't correspond to frames, and then starts frame number at zero and increases by 1 each row.

--- R/get_blackfly_timestamp_from_vidtime.R
+++ R/get_blackfly_timestamp_from_vidtime.R
@@ -1,10 +1,10 @@
 #' Returns Blackfly Timestamp based on Video Number and Seconds in
 #'
-#' Returns a vector of Blackfly Timestamps corresponding to video_col and video_sec by using those two parameters to approximate frame number and then mathc that frame number to a table to see when that frame was recorded.
+#' Returns a vector of Blackfly Timestamps corresponding to video_col and video_sec by using those two parameters to approximate frame number and then match that frame number to a table to see when that frame was recorded. Timestamps and filepaths vectors must correspond to eachother and be in the order that the frames were recorded
 #' @param video_col vector of video numbers (must be same length as seconds_col)
 #' @param seconds_col vector of seconds in video (must be same length as video_col)
 #' @param cam_timestamp vector of camera timestamps (must be same length as cam_filepath)
-#' @param cam_filepath vector of filepaths from balckfly camera (must be same length as cam_timestamp)
+#' @param cam_filepath vector of filepaths from blackfly camera (must be same length as cam_timestamp)
 #' @import dplyr
 #' @import magrittr
 #' @import tibble
@@ -18,8 +18,7 @@ get_blackfly_timestamp_from_vidtime<- function(video_col,seconds_col,cam_timesta
   cam<- bind_cols(as.tibble(cam_timestamp), as.tibble(cam_filepath))
   names(cam)<- c("cam_timestamp", "cam_filepath")
   cam<- cam %>% filter(grepl(pattern = "GigEGrabEx-[\\d]*", x = cam$cam_filepath)) #remove any rows without GigEGrabEx
-  cam<- cam %>% mutate(gigegrab= str_extract(cam$cam_filepath,"GigEGrabEx-[\\d]*"))
-  cam<- cam %>% mutate(gigegrab_num=as.numeric(str_extract(cam$gigegrab, "\\d+"))) %>% mutate(frame_num=gigegrab_num-gigegrab_num[1]) #Extract frame number from filename accounting for if gigegrab_num doesn't start at 0
+  cam<- cam %>% mutate(frame_num=0:(nrow(cam)-1)) #Increase frame by 1 with row
   both<- left_join(video, cam, by="frame_num") #Join video and cam by frame number
   output<- both$cam_timestamp #Return vector of timestamps
   return(output)
