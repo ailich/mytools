ca69fbc415cc4b372826e10eaf499ccd58ce5aee
Alex Ilich <ailich@mail.usf.edu>
2019-06-26 16:52:37 -0400

    check_camera_drift update -can account for camera resets internally -better handling of missing sundirs -safer b/c records skipped transects and reasons why they were skipped (Does not record random ones that didn't even have right starting directories though)

--- R/check_camera_drift.R
+++ R/check_camera_drift.R
@@ -42,7 +42,7 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
     names(fixed_tables)<- c(transect_dir, sub_dirs)
     fixed_tables<- fixed_tables %>% mutate(full_dir = paste(transect_dir, sub_dirs, sep="_"))}
 
-  output<- tibble(Transect=NA_character_, st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=NA_integer_, j= NA_integer_, table_issue = NA_character_, n_frames_diff = NA_integer_, correction_factor=NA_real_,transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= NA_real_, table_path = NA_character_)[0,]
+  output<- tibble(Transect=NA_character_, st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=NA_integer_, j= NA_integer_, table_issue = NA_character_, n_frames_diff = NA_integer_, correction_factor=NA_real_,transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= NA_real_, table_path = NA_character_, skip_reason = NA_character_)[0,]
 
   CBASS_transects<- list.files(CBASS_dir) #Get all CBASS Transect Names from Cruise
   CBASS_transects<- CBASS_transects[!grepl(pattern = "\\.", CBASS_transects)] #Removes non transect files (e.g. pdf files) from transect list
@@ -56,16 +56,21 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
       warning(paste(transect_dir, "does not have necessary directories"))
       next}
     list.files(transect_dir)
-    sub_dirs<- list.files(paste(transect_dir, camera_folder_name, sep="/"))
+    sub_dirs<- list.files(paste(transect_dir, camera_folder_name, sep="/")) #Subdirs of camera folder
     sub_dirs<- sub_dirs[!grepl(pattern = "\\.", sub_dirs)] #Remove stray files
+    sub_dirs2<- list.files(paste(transect_dir, "tables", sep="/")) #Subdirs of table folder
+    sub_dirs2<- sub_dirs2[!grepl(pattern = "\\.", sub_dirs)] #Remove stray files
 
     if(length(sub_dirs)==0){sub_dirs=""} #Deal with no subdirs
     sub_dirs
+    if(length(sub_dirs2)==0){sub_dirs2=""} #Deal with no subdirs
     for (j in 1:length(sub_dirs)) { #Loop through subdirectories
       print(paste("    ", "subdirectory", sub_dirs[j]))
       vid_files<- sort(list.files(path = paste(transect_dir, camera_folder_name, sub_dirs[j], sep="/"), pattern = "\\.avi$", full.names = TRUE))
       if(length(vid_files)==0){
         warning("no videos in dir")
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=NA_character_, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames =NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "No Videos")
+        output<- bind_rows(output, new_row)
         next}
 
       pic_files<- sort(list.files(path = paste(transect_dir, camera_folder_name, sub_dirs[j], sep="/"), pattern = "\\.jpg$", full.names = TRUE))
@@ -77,15 +82,23 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
         table_dir<- paste(transect_dir, "tables", sub_dirs[j], sep="/")
         curr_table_name<- table_name}
       curr_table_name
-      if ((!file.exists(table_dir)) & (length(sub_dirs)==1)){
-        table_dir<- paste(transect_dir, "tables", sep="/")
-      } #Account for if no subdir in table folder (Feb 2016)
+      if ((!file.exists(table_dir)) & sub_dirs2[j]=="" & length(sub_dirs==1) & length(sub_dirs2)==1){
+        table_dir<- paste(transect_dir, "tables", sep="/") #Account for if no subdir in table folder (Feb 2016)
+      } else if (!(sub_dirs[j] %in% sub_dirs2)){ #Make sure subdirs of tables and camera folder match
+        warning(paste("mismatched subdirs"))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=NA_character_, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames =NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "mismatched subdirs")
+        output<- bind_rows(output, new_row)
+        next}
       if(sum(grepl(pattern= curr_table_name, x = list.files(table_dir)))==0){
         warning(paste("no", curr_table_name))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=NA_character_, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames =NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "No Table")
+        output<- bind_rows(output, new_row)
         next}
       pic_table<- read_tsv(paste(table_dir, curr_table_name, sep="/"), col_names =  has_colnames, col_types = "Tdc")
       if(nrow(pic_table)==0){
         warning(paste(curr_table_name, "is empty"))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=NA_character_, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames =NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "Table has zero rows")
+        output<- bind_rows(output, new_row)
         next}
       if (!has_colnames){
         names(pic_table)<- c("timestamp", "u_second",  "file_path")
@@ -93,16 +106,27 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
 
       table_issue<- check_camera_table(pic_table, display_warning = FALSE)
       table_issue
-      if ((table_issue =="reset") | (table_issue =="pattern")){
+      if (table_issue =="pattern"){
         warning(paste(curr_table_name, "is", table_issue))
-        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames =NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames =NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "table pattern issue")
         output<- bind_rows(output, new_row)
         next
       }
-      if (table_issue =="jumbled"){warning(paste(curr_table_name, "is", table_issue))}
-
       my_pattern<- detect_cam_pattern(pic_table$file_path)
       my_pattern
+      if (table_issue =="reset" & sub_dirs[j]!=""){
+        warning(paste(curr_table_name, "is", table_issue))
+        pic_table<- pic_table %>% filter(grepl(pattern= sub_dirs[j], x = pic_table$file_path))
+      } #Filter pic table to deal with resets based on subdir name
+
+      if (table_issue =="reset" & sub_dirs[j]==""){
+        warning(paste(curr_table_name, "is", table_issue, "and was reset"))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= NA_integer_, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= NA_real_, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "reset with no subdir name")
+        output<- bind_rows(output, new_row)
+        next} #Cannot Filter pic table to deal with resets without subdir name
+
+      if (table_issue =="jumbled"){warning(paste(curr_table_name, "is", table_issue))}
+
       pic_table2<- pic_table %>% filter(grepl(pattern = my_pattern, x = file_path))
       pic_table2<- pic_table2 %>% mutate(exact_time = timestamp+ microseconds(u_second))
       pic_table2<- pic_table2 %>% mutate(diff=NA_real_)
@@ -116,7 +140,7 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
       n_frames_diff
       if(n_frames_diff!=0){
         warning("n_frames in table is not equal to n_frames in pic dir")
-        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= n_frames_diff, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= max_frame_diff, table_path = paste(table_dir, curr_table_name, sep="/"))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= n_frames_diff, correction_factor=NA_real_, transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= max_frame_diff, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "n_frames_diff")
         output<- bind_rows(output, new_row)
         next
       }
@@ -138,7 +162,7 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
 
       if(!str_extract(string = pic_table$file_path[end_idx], pattern = my_pattern)== str_extract(string = pic_files[length(pic_files)], pattern = my_pattern)) {#Check that last frame in picture folder matches with table
         warning("last_pic in table is not the same as last in picture dir")
-        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= n_frames_diff, correction_factor=NA_real_,transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= max_frame_diff, table_path = paste(table_dir, curr_table_name, sep="/"))
+        new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= NA, end_time = NA, Vid_drift=NA_real_, fps_mosaic = NA_real_, fps_rec = NA_real_, i=i, j=j, table_issue=table_issue, n_frames_diff= n_frames_diff, correction_factor=NA_real_,transect_dur_mins = NA_real_, vid_dur_mins = NA_real_, n_frames=NA_integer_, max_frame_diff= max_frame_diff, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason = "last frame name mismatch")
         output<- bind_rows(output, new_row)
         next}
 
@@ -164,7 +188,7 @@ check_camera_drift<- function(CBASS_dir, camera_folder_name= "blackfly", table_n
       correction_factor<- transect_dur_secs/vid_dur_secs
       Vid_drift<- vid_dur_secs - transect_dur_secs
 
-      new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= st_time, end_time = end_time_real, Vid_drift = Vid_drift, fps_mosaic = fps_mosaic, fps_rec = fps_rec, i=i, j=j, table_issue=table_issue, n_frames_diff= n_frames_diff, correction_factor=correction_factor, transect_dur_mins = transect_dur_secs/60, vid_dur_mins = vid_dur_secs/60, n_frames=n_frames_adj, max_frame_diff= max_frame_diff, table_path = paste(table_dir, curr_table_name, sep="/"))
+      new_row<- tibble(Transect=paste0(CBASS_transects[i], "_", sub_dirs[j]), st_time= st_time, end_time = end_time_real, Vid_drift = Vid_drift, fps_mosaic = fps_mosaic, fps_rec = fps_rec, i=i, j=j, table_issue=table_issue, n_frames_diff= n_frames_diff, correction_factor=correction_factor, transect_dur_mins = transect_dur_secs/60, vid_dur_mins = vid_dur_secs/60, n_frames=n_frames_adj, max_frame_diff= max_frame_diff, table_path = paste(table_dir, curr_table_name, sep="/"), skip_reason=NA_character_)
       output<- bind_rows(output, new_row)
     }
   }
