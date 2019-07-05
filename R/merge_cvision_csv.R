3b487be33c360d0646f2d3660385c93acb7812f9
Alex Ilich <ailich@mail.usf.edu>
2019-07-05 13:30:40 -0400

    merge_cvision_csv bug fix -New names for axis cameras added " ax#-" at front of file name. When extracting video number from the file name it was getting the axis number. Now removes that then grabs other number. -If after removing axis camera number there are more than two numbers available as options for video, an error will be thrown - Now checks to see if csvs in file list all have correct column names and throws error if they don't

--- R/merge_cvision_csv.R
+++ R/merge_cvision_csv.R
@@ -11,29 +11,35 @@
 #' @import readr
 
 merge_cvision_csv<- function(file_list=list.files(pattern = "\\.csv$"), frames_per_sec, vid_length){
-  fish<- suppressWarnings(suppressMessages(read_csv(file = file_list[1],col_types=cols(.default="c", Trip_ID="i", Tow_Number="i", Fish_Number="i", Frame="i", Time_In_Video="d"))))
-  fish<- fish[,1:9]
-  fish<- fish %>% mutate(file_name= file_list[1])
-  if(length(file_list)>1){
-    for (i in 2:length(file_list)){
-      new_fish<- suppressWarnings(suppressMessages(read_csv(file = file_list[i],col_types=cols(.default="c", Trip_ID="i", Tow_Number="i", Fish_Number="i", Frame="i", Time_In_Video="d"))))
-      new_fish<- new_fish[,1:9]
-      new_fish<- new_fish %>% mutate(file_name= file_list[i])
-      fish<- bind_rows(fish,new_fish)
-      rm(i,new_fish)
-    }}
-  fish<- fish %>% select(file_name, Reviewer, Fish_Number, Fish_Type, Time_In_Video, Frame)
-  fish$short_filename<- NA_character_
-  for (i in 1:nrow(fish)) {
-    idx<- gregexpr("\\/", fish$file_name[i])[[1]]
-    start_pos<- idx[length(idx)]+1
-    end_pos<- nchar(fish$file_name[i])
-    fish$short_filename[i]<-substr(x = fish$file_name[i], start = start_pos, stop=end_pos)
+  fish<- tibble("Trip_ID" =NA_integer_, "Tow_Number"= NA_integer_, "Reviewer"=NA_character_, "Tow_Type"=NA_character_, "Fish_Number"=NA_integer_, "Fish_Type"=NA_character_, "Species"=NA_character_, "Frame"=NA_integer_, "Time_In_Video"=NA_real_, file_name=NA_character_) #Initialize accumulator
+  for (i in 1:length(file_list)){
+    new_fish<- suppressWarnings(suppressMessages(read_csv(file = file_list[i],col_types=cols(.default="c", Trip_ID="i", Tow_Number="i", Fish_Number="i", Frame="i", Time_In_Video="d"))))
+    new_fish<- new_fish[,1:9]
+    if(!identical(names(new_fish), c("Trip_ID", "Tow_Number", "Reviewer", "Tow_Type", "Fish_Number", "Fish_Type", "Species", "Frame", "Time_In_Video"))){
+      message(paste("Columns of file",  file_list[i], "not as expected"))
+      stop()} #Throw error if col names not as expected
+    new_fish<- new_fish %>% mutate(file_name= file_list[i])
+    fish<- bind_rows(fish,new_fish)
+    rm(i,new_fish)
   }
-  fish<- fish %>% mutate(video= as.numeric(str_extract(string = short_filename,pattern = "\\d+")))
+  fish<- fish %>% select(file_name, Reviewer, Fish_Number, Fish_Type, Time_In_Video, Frame)
+  fish<- fish %>% mutate (short_filename =basename(file_name))
+  fish<- fish %>% mutate (video =NA_real_) #Initialize video column
+
+  for (j in 1:nrow(fish)) {
+    curr_vid_num<- fish$short_filename[j] %>%
+      str_remove(pattern = "ax\\d-") %>% #Remove axis number
+      str_extract_all(pattern = "\\d+", simplify = TRUE)
+    if(length(curr_vid_num)==1){
+      fish$video[j]<- as.numeric(curr_vid_num)} else{
+        message(paste("Error: Could not extract video number from file name", fish$short_filename[j]))
+        stop()}
+    rm(j, curr_vid_num)
+  }
   fish<- fish %>% mutate(Total_Frame= video*(frames_per_sec*(vid_length*60))+Frame)
   fish<- fish %>% mutate(Total_Time_In= (video*(vid_length*60))+Time_In_Video)
   fish<- fish %>% select(file_name,video,Reviewer,Fish_Number,Fish_Type,Time_In_Video, Frame, Total_Time_In, Total_Frame)
+  fish<- fish[-1,] #Remove first row of NA's
   return(fish)
 }
 
