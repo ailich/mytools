8795df637e79c99090c20a77a7dc43b05d3c236a
Alex Ilich <ailich@mail.usf.edu>
2019-09-06 16:22:09 -0400

    make_crs minor update - can now exlude epsg at beginning of proj4 string in output - our shapefile and raster data often does not specify this and it causes identicalCRS to fail even though they are really the same crs

--- R/make_crs.R
+++ R/make_crs.R
@@ -5,12 +5,15 @@
 #' "UTM 16N": datum WGS84, projection = UTM 16N
 #' "UTM 17N": datum WGS84, projection = UTM 17N
 #' @param x desired coordinate system/datum
+#' @param exclude_epsg logical indicating whether to exclude +init=epsg: at beginning of proj4 string
 #' @import sp
+#' @importFrom stringr str_remove
 #' @export
 
-make_crs<- function(x){
+make_crs<- function(x, exclude_epsg=FALSE){
   if (x == "WGS84") {out<- CRS("+init=epsg:4326")}
   if (x == "UTM 16N") {out<- CRS("+init=epsg:32616")}
   if (x == "UTM 17N") {out<- CRS("+init=epsg:32617")}
+  if(exclude_epsg){out<- CRS(str_remove(out, "^\\+init=epsg:\\d+"))}
   return(out)
 }
