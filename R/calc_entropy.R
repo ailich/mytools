7ecd7418338e0e37a3685c76d1f7708c962d22ed
Alex Ilich <ailich@mail.usf.edu>
2019-10-11 13:32:58 -0400

    calc_entropy update -raster package bug seems to put NA instead of -inf in certain cases for large rasters. Added line to account for this bug.

--- R/calc_entropy.R
+++ R/calc_entropy.R
@@ -9,5 +9,6 @@ calc_entropy<- function(raster_stack, base=exp(1)){
   pLog<- log(raster_stack, base=base)
   pLog[pLog==-Inf]<- 0
   raster_entropy<- -1*sum(raster_stack*pLog)
+  raster_entropy[min(raster_stack)==0]<- 0 #In case pLog[pLog == -Inf] <- 0 which it seems to for large rasters
   return(raster_entropy)
 }
